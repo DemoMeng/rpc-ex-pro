f9644cd56aeb9fd3b26c1583780e39859889038c
蒙启章 <mengqizhang@huluwa.cc>
2020-09-15 08:47:39 +0800

    remove the module that named rpc-zk

--- rpc-producer/src/main/java/com/rpc/producer/service/ProducerService.java
+++ rpc-producer/src/main/java/com/rpc/producer/service/ProducerService.java
@@ -7,6 +7,8 @@ import com.rpc.api.service.CommonService;
  * @author mqz
  * @description
  * @since 2020/9/14
+ * 注意：
+ *   这里使用的是DubboService  -> dubbo的版本该注解命名为Service(com.alibaba.dubbo)，随后改成 DubboService(com.apache.dubbo)
  */
 @Service(version = "${rpc.version}")
 public class ProducerService implements CommonService {
